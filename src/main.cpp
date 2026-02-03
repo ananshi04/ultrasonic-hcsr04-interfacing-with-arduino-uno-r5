4ae087c0c698afbfd9add9200cbb8f526367d8d1
ananshi <ananshi.gla_cs.aiml24@gla.ac.in>
2026-02-03 09:21:18 +0530

    Add initial project configuration files and README documentation

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,20 @@
 #define echoPin 10
 void setup() {
 // write your initialization code here
-    pinMode(pinNumber:trigPin,pinMode:OUTPUT);
-    pinMode(pinNumber:echoPin,pinMode:INPUT);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
 }
-
 void loop() {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    long duration = pulseIn(echoPin, HIGH);
+    float distance = duration * 0.034 / 2;
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+    delay(500);
 // write your code here
 }
