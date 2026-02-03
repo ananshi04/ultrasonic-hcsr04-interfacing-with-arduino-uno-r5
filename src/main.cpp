5c73794f617129835c8bdce74033505d58fb975e
ananshi <ananshi.gla_cs.aiml24@gla.ac.in>
2026-02-03 09:46:00 +0530

    Initialize serial communication and configure upload/monitor ports

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ void setup() {
 // write your initialization code here
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
+    Serial.begin(9600);
 }
 void loop() {
     digitalWrite(trigPin, LOW);
