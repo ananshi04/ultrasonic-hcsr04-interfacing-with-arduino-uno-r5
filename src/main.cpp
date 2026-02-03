28567fa7458d48d195e5cbb5f70dada594cec55e
ananshi <ananshi.gla_cs.aiml24@gla.ac.in>
2026-02-03 09:12:36 +0530

    Add initial project configuration files and README documentation

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#define trigPin 9
+#define echoPin 10
+void setup() {
+// write your initialization code here
+    pinMode(pinNumber:trigPin,pinMode:OUTPUT);
+    pinMode(pinNumber:echoPin,pinMode:INPUT);
+}
+
+void loop() {
+// write your code here
+}
